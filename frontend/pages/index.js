dbca933f2c6ad7eca38cbac0c72e063bf228c9af
6532swornim <swornimdai54@gmail.com>
2025-06-26 00:59:24 +0545

    Updtae

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -17,68 +17,86 @@ export default function Dropdown() {
   };
 
   return (
-    <nav className={styles.nav}>
-      <img
-        src="https://imgs.search.brave.com/W9J42-2dzJU6-Egw26SmEcuJMX-gannKDRkPL0Sxgbk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTU1/MzUyODQ5L3Bob3Rv/L3R3by1ldXJvLWNv/aW4uanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPTNkM0tSOThG/ZWpnREd4NExYLWx4/ZlN4UTVGTjJPcUlf/eks0VDE1LWtVdjQ9"
-        alt="logo"
-      ></img>
-      <div className={styles.home}>
-        <div>
-          <a href="#">Home</a>
-          <a href="#" style={{ marginLeft: "20px" }}>
-            About us
-          </a>
-        </div>
-        <div className={styles.dropdown}>
-          <button
-            className={styles.dropbtn}
-            onClick={toggleGallery}
-            aria-haspopup="true"
-            aria-expanded={isGalleryOpen}
-          >
-            Gallery ▼
-          </button>
+    <>
+     <nav className={styles.nav}>
+        <img
+          src="https://imgs.search.brave.com/W9J42-2dzJU6-Egw26SmEcuJMX-gannKDRkPL0Sxgbk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTU1/MzUyODQ5L3Bob3Rv/L3R3by1ldXJvLWNv/aW4uanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPTNkM0tSOThG/ZWpnREd4NExYLWx4/ZlN4UTVGTjJPcUlf/eks0VDE1LWtVdjQ9"
+          alt="logo"
+        ></img>
+        <div className={styles.home}>
+         
+            <a href="#">Home</a>
+            <a href="#" style={{ marginLeft: "20px" }}>
+              About us
+            </a>
+             <a href="#" style={{ marginLeft: "20px" }}>
+             Testmonial
+            </a>
+          <div className={styles.dropdown}>
+            <button
+              className={styles.dropbtn}
+              onClick={toggleGallery}
+              aria-haspopup="true"
+              aria-expanded={isGalleryOpen}
+            >
+              Gallery ▼
+            </button>
 
-          {isGalleryOpen && (
-            <ul className={styles.dropdownContent} role="menu">
-              <li role="menuitem">
-                <a href="#">Photos</a>
-              </li>
-              <li role="menuitem">
-                <a href="#">Videos</a>
-              </li>
-              <li role="menuitem">
-                <a href="#">Artworks</a>
-              </li>
-            </ul>
-          )}
-        </div>
+            {isGalleryOpen && (
+              <ul className={styles.dropdownContent} role="menu">
+                <li role="menuitem">
+                  <a href="#">Photos</a>
+                </li>
+                <li role="menuitem">
+                  <a href="#">Videos</a>
+                </li>
+                <li role="menuitem">
+                  <a href="#">Artworks</a>
+                </li>
+              </ul>
+            )}
+          </div>
 
-        <div className={styles.dropdown}>
-          <button
-            className={styles.dropbtn}
-            onClick={toggleDestinations}
-            aria-haspopup="true"
-            aria-expanded={isDestinationsOpen}
-          >
-            Destinations ▼
-          </button>
+          <div className={styles.dropdown}>
+            <button
+              className={styles.dropbtn}
+              onClick={toggleDestinations}
+              aria-haspopup="true"
+              aria-expanded={isDestinationsOpen}
+            >
+              Destinations ▼
+            </button>
 
-          {isDestinationsOpen && (
-            <ul className={styles.dropdownContent} role="menu">
-              <li role="menuitem">
-                <a href="#">Europe</a>
-              </li>
-              <li role="menuitem">
-                <a href="#">Asia</a>
-              </li>
-              <li role="menuitem">
-                <a href="#">America</a>
-              </li>
-            </ul>
-          )}
+            {isDestinationsOpen && (
+              <ul className={styles.dropdownContent} role="menu">
+                <li role="menuitem">
+                  <a href="#">Europe</a>
+                </li>
+                <li role="menuitem">
+                  <a href="#">Asia</a>
+                </li>
+                <li role="menuitem">
+                  <a href="#">America</a>
+                </li>
+              </ul>
+            )}
+             <a href="#">
+              Contact us 
+            </a>
+          </div>
         </div>
-      </div>
     </nav>
+  <p>lorem*asdlkfjklasdjfkl;ajsldkflsadf
+    asdlkfjsakldjfkl;sajd;lfjdsa;kjsadfasdkfjsadjkl;jadsjfffffffffffffffffffffffffffffffffffffjdkjfkdjfkjdkfjdkfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjdd
+    dfjkljdlfjldsja;sdjf;lsajflk;sajflkajsdlfjsa;kjfssssssfasdklfjklsa;jadsjfffffffffffffffffffffffffffffffffffffjdkjfkdjfkjdkfjdkfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjddsadfklsaj;lkjklsa;djf;as\dfjkljdlfjldsjaasjkdfk;lsaj;falsesadlkfja;lkdf
+    asdlkfjaslkjflk;asj;dfjkljdlfjldsjasadklfj;lkasdjflasd
+    sadkjfaksldjfl;jadsjfffffffffffffffffffffffffffffffffffffjdkjfkdjfkjdkfjdkfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjddfklsadjf;lksajdlk;fjlsad
+    fsadlkfjaklsjfkl;sajflkajsdlfjsasadjklfklas;jdfklad f jfklsadj;lfkjasl; lksajdlk fklsda fjklsaj fklsajdlkfj asdlkfjaslkjflkf sakldfj k;lsajf lkjsdalkf
+    safkjsadhf sad fklsdjf;j sadkjfaksldjflasdklfjsakl jfsa j klfjlsda f
+    as klfjsald fjlsad ;lkf jsad 
+    sakld jlksdj flsjadk;fklsajdlkfj
+  </p>
+  <img src="https://geletrekking.com/wp-content/uploads/2024/05/Traditional-Load-Carrying-in-Nepal-768x1024.jpg"></img>
+</>
   );
 }
