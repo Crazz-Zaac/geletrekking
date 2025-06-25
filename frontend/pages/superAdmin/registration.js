9533b079e7079718cc617c5091714c6b887e8a85
6532swornim <swornimdai54@gmail.com>
2025-06-26 01:05:38 +0545

    update

--- frontend/pages/superAdmin/registration.js
+++ frontend/pages/superAdmin/registration.js
@@ -5,8 +5,15 @@ export default function LoginPage() {
   const router = useRouter();
   return (
     <form onSubmit={handleSubmit}>
+      <input type="text" name="username" placeholder="username" required />
+
       <input type="email" name="email" placeholder="Email" required />
+
+      <input type="text" name="phone_no" placeholder="phonr_no" required />
+
       <input type="password" name="password" placeholder="Password" required />
+      <input type="password" name="confirmpassword" placeholder="confirm password" required />
+
       <button type="submit">Login</button>
     </form>
   );
