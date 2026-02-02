4901155217b31c872044a53568cd88a27659ea37
daydevil80 <ldawa9808@gmail.com>
2026-02-02 09:45:33 +0900

    created .env and all crediantial store in that and didn't add anything in yml instead i add .env file in docker folder - closes # 14

--- backend/superadminSeeder.js
+++ backend/superadminSeeder.js
@@ -4,29 +4,28 @@ const bcrypt = require('bcrypt');
 const User = require('./models/user');
 
 dotenv.config();
+
 const MONGO_URI = process.env.MONGO_URI;
+const SUPERADMIN_EMAIL = process.env.SUPERADMIN_EMAIL;
+const SUPERADMIN_PASSWORD = process.env.SUPERADMIN_PASSWORD;
 
 const seedSuperAdmin = async () => {
   try {
     await mongoose.connect(MONGO_URI);
     
-    const existing = await User.findOne({ email: 'superadmin@geletrekking.com' });
-
+    const existing = await User.findOne({ email: SUPERADMIN_EMAIL });
     if (existing) {
       console.log('Superadmin already exists');
     } else {
-      const hashedPassword = await bcrypt.hash('f   ', 10);
-
+      const hashedPassword = await bcrypt.hash(SUPERADMIN_PASSWORD, 10);
       await User.create({
         name: 'Super Admin',
-        email: 'superadmin@geletrekking.com',
+        email: SUPERADMIN_EMAIL,
         password: hashedPassword,
         role: 'superadmin'
       });
-
       console.log('✅ Superadmin created');
     }
-
     process.exit();
   } catch (error) {
     console.error('❌ Seeding failed:', error.message);
